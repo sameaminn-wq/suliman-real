17382b20067f630b02de694ec20ba837909106ef
sameaminn-wq <sameaminn@gmail.com>
2026-04-27 13:30:28 +0300

    2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    experimental: {
+      serverActions: true, // هذا السطر هو الحل للمشكلة
+    },
+    // أي إعدادات أخرى لديك اتركها كما هي
+  };
+  
+  module.exports = nextConfig;
